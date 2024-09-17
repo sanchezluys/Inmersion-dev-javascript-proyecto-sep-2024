2558c02fa877acc62e989d2b2764ffa8cd6f4dc2
luys <sanchezluys@gmail.com>
2024-09-17 10:27:48 -0500

    first commit

---
+++ inmersion-dev-aula1-Proyecto-Base/main.js
@@ -0,0 +1,20 @@
+// declarando las variables
+let cantidad = document.getElementById("cantidad");
+let texto ="texto";
+
+// tipo de dato
+console.log(typeof cantidad);
+console.log(typeof texto);  
+
+if (cantidad < 6 ){
+    console.log("La cantidad es menor a 6");
+}
+else if (cantidad > 6 && cantidad < 10 ){
+    console.log("La cantidad es mayor a 6 y menor a 10");
+}
+
+
+
+
+
+
