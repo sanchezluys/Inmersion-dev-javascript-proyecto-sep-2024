94cb229454db5bf2cf6019e429a6dbeb06d9b0db
luys <sanchezluys@gmail.com>
2024-09-17 13:00:20 -0500

    agregando funcion

--- inmersion-dev-aula1-Proyecto-Base/main.js
+++ inmersion-dev-aula1-Proyecto-Base/main.js
@@ -1,19 +1,34 @@
 // declarando las variables
+//let cantidad= 1;
 let cantidad = document.getElementById("cantidad");
 let texto ="texto";
+// boton generar
+
+let boton = document.getElementById("generar");
 
 // tipo de dato
 console.log(typeof cantidad);
 console.log(typeof texto);  
 
-if (cantidad < 6 ){
-    console.log("La cantidad es menor a 6");
-}
-else if (cantidad > 6 && cantidad < 10 ){
-    console.log("La cantidad es mayor a 6 y menor a 10");
+
+function generar(){
+    // en la clase usan: pasrseint(cantidad.value);
+    let numeroDigitado=parseInt(cantidad.value);
+    console.log(numeroDigitado);
+    if (numeroDigitado < 6 ){
+        console.log("La cantidad es menor a 6");
+        alert("error la cantidad debe ser mayor que 6");
+    }
+    else if (numeroDigitado >= 6 ){
+        console.log("La cantidad es mayor o igual a 6");
+        alert("Muy bien cantidad acepata!!");
+    }
 }
 
 
+
+
+console.log("fin del programa");
 
 
 
