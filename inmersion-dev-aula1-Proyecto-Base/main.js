ee49b2ee7f008250697a72fba69b7a9c399ec141
luys <sanchezluys@gmail.com>
2024-09-17 15:12:11 -0500

    programa funcionando

--- inmersion-dev-aula1-Proyecto-Base/main.js
+++ inmersion-dev-aula1-Proyecto-Base/main.js
@@ -3,14 +3,14 @@
 let cantidad = document.getElementById("cantidad");
 let texto ="texto";
 // boton generar
-
 let boton = document.getElementById("generar");
-
 // tipo de dato
 console.log(typeof cantidad);
 console.log(typeof texto);  
-
-
+// constantes
+const cadenaCaracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+// contraseña
+let contrasena = document.getElementById("contrasena");
 function generar(){
     // en la clase usan: pasrseint(cantidad.value);
     let numeroDigitado=parseInt(cantidad.value);
@@ -23,13 +23,15 @@ function generar(){
         console.log("La cantidad es mayor o igual a 6");
         alert("Muy bien cantidad acepata!!");
     }
+    let password = '';
+    for (let index = 0; index < numeroDigitado; index++) {
+        
+        let caracterAleatorio= cadenaCaracteres[ Math.floor(Math.random() * cadenaCaracteres.length)];
+
+        password+= caracterAleatorio;
+        console.log(caracterAleatorio);
+        console.log(password);
+    }
+    contrasena.value=password;
 }
-
-
-
-
 console.log("fin del programa");
-
-
-
-
